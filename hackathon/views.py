bef8523d8049076e13f8eedc5de5f1b15fc118ca
s3782490 <s3782490@student.rmit.edu.au>
2022-06-20 14:09:45 +1000

    Addded bootstrap and initial html file

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the hackaTHON index.")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())

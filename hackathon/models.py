839f19fb113c66663581e55c84b51cf76541b52d
benwest03 <77653862+benwest03@users.noreply.github.com>
2022-06-20 15:03:53 +1000

    Btn update

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,3 +1,24 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    stock_code = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50) 
+    salary_band = models.CharField(max_length=100)
+    Q1 = models.IntegerField(default=-1)
+    Q2 = models.IntegerField(default=-1)
+    Q3 = models.IntegerField(default=-1)
+    Q4 = models.IntegerField(default=-1)
+    Q5 = models.IntegerField(default=-1)
+    Q6 = models.IntegerField(default=-1)
+    Q7 = models.IntegerField(default=-1)
+    Q8 = models.IntegerField(default=-1)
+    Q9 = models.IntegerField(default=-1)
+    Q10 = models.IntegerField(default=-1)
+
+class Companies(models.Model):
+    name = models.CharField(max_length=100)
+    stock_code = models.ForeignKey(Users ,on_delete=models.CASCADE)
--- hackathon_site/settings.py
+++ hackathon_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hackathon.apps.HackathonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

8e8ff90409cbf2dd7902f73a3dbd9e3d09d2b54e
benwest03 <77653862+benwest03@users.noreply.github.com>
2022-06-20 14:54:20 +1000

    Added Simple Navbar

---
+++ convertcsv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+dict_from_csv = pd.read_csv(
+    'ASX 300 Data.csv', header=None, index_col=0, squeeze=True).to_dict()
+print(dict_from_csv)
+
+for item in dict_from_csv:
+    print("INSERT INTO *tablename (stock_code, company_anem) VALUES ({},{});".format(item, dict_from_csv[item]))
+
+

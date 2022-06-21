80fc775694e1fd772f1c4614014ed595787d255e
Kai Gibson <kai.gib@tuta.io>
2022-06-21 11:13:24 +1000

    form interacting with db

--- hackathon/views.py
+++ hackathon/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 from .forms import InputForm
+from .models import Companies, Users
 
 # Create your views here.
 from django.http import HttpResponse
@@ -17,9 +18,23 @@ def user_form(request):
         form = InputForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
-            for i in form.cleaned_data:
-                print(i)
+
+            company = Companies.objects.get(stock_code='AGL')
+
+            print(company.stock_code)
+            user = Users()
+            user.stock_code = company
+            user.Q1 = form.cleaned_data['question_1']
+            user.Q2 = form.cleaned_data['question_2']
+            user.Q3 = form.cleaned_data['question_3']
+            user.Q4 = form.cleaned_data['question_4']
+            user.Q5 = form.cleaned_data['question_5']
+
+            user.save()
+
+            # print(form.cleaned_data)
+            # for i in form.cleaned_data:
+            #    print(i)
             return HttpResponseRedirect('/admin/')
     else:
         context = {}

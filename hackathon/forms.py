5da7e33d35850e2ae8ba914dab18802f41830462
Kai Gibson <kai.gib@tuta.io>
2022-06-21 13:00:39 +1000

    kai push

--- hackathon/forms.py
+++ hackathon/forms.py
@@ -1,6 +1,7 @@
 from sre_constants import CH_LOCALE
 from click import Choice
 from django import forms
+from django.forms import widgets
 
 
 CHOICES = [('one', 'One'), ('two', 'Two')]
@@ -9,53 +10,150 @@ CHOICES = [('one', 'One'), ('two', 'Two')]
 
 
 class InputForm(forms.Form):
+
+    company_name = forms.CharField(label="Select Company Name",
+                                   widget=forms.Select(choices=[
+                                       ('AGL', 'AGL Energy Ltd'),
+                                       ('ALU', 'Altium'),
+                                       ('CSL', 'CSL'),
+                                       ('APT', "Afterpay")
+                                   ]
+                                   )
+                                   )
+
+    role = forms.CharField(label="Please select your job title",
+                           widget=forms.Select(choices=[
+                               ('software developer', 'Software Developer'),
+                               ('lead engineer', 'Lead Engineer'),
+                               ('junior engineer', 'Junior Engineer'),
+                               ('qa tester', 'QA Tester')]
+                           )
+                           )
+    gender = forms.CharField(label="Please select your Gender",
+                             widget=forms.Select(choices=[
+                                 ('male', 'Male'),
+                                 ('female', 'Female'),
+                                 ('non-binary', 'Non-Binary'),
+                                 ('other', 'Other/Prefer not to say')]
+                             )
+                             )
+
+    salary_band = forms.CharField(label="Please select your salary band",
+                                  widget=forms.Select(choices=[
+                                      ('60-79k', '60-79k'),
+                                      ('80-99k', '80-99k'),
+                                      ('100-109k', '100-109k'),
+                                      ('110-119k', '110-119k'),
+                                      ('120-129k', '120-129k'),
+                                      ('130-139k', '130-139k'),
+                                      ('140-149k', '140-149k'),
+                                      ('150-159k', '150-159k'),
+                                      ('160-169k', '160-169k'),
+                                      ('170-179k', '170-179k'),
+                                      ('180k+', '180k+'),
+                                  ]
+                                  )
+                                  )
+
     question_1 = forms.ChoiceField(label="Q1: How often have you ever witnessed gender-based discrimination in the workplace?",
                                    choices=[
-                                       ('never', 'Never'),
-                                       ('rarely', 'Rarely'),
-                                       ('sometimes', 'Sometimes'),
-                                       ('frequently', 'Frequently'),
-                                       ('often', 'Often'),
-                                       ('daily', 'Daily')],
+                                       (1, 'Never'),
+                                       (2, 'Rarely'),
+                                       (3, 'Sometimes'),
+                                       (4, 'Frequently'),
+                                       (5, 'Often'),
+                                       (6, 'Daily')],
                                    widget=forms.RadioSelect
                                    )
+
     question_2 = forms.ChoiceField(label="Q2: How often do you or your coworkers feel unsafe at work?",
                                    choices=[
-                                       ('never', 'Never'),
-                                       ('rarely', 'Rarely'),
-                                       ('sometimes', 'Sometimes'),
-                                       ('frequently', 'Frequently'),
-                                       ('often', 'Often'),
-                                       ('daily', 'Daily')],
+                                       (1, 'Never'),
+                                       (2, 'Rarely'),
+                                       (3, 'Sometimes'),
+                                       (4, 'Frequently'),
+                                       (5, 'Often'),
+                                       (6, 'Daily')],
                                    widget=forms.RadioSelect
                                    )
+
     question_3 = forms.ChoiceField(label="Q3: Are promotions distributed on merit rather than gender?",
                                    choices=[
-                                       ('no', 'Not at all'),
-                                       ('notreally', 'Not Really'),
-                                       ('somewhat', 'Somewhat'),
-                                       ('relatively', 'Relatively'),
-                                       ('mostly', 'Mostly'),
-                                       ('yes', 'Yes')],
+                                       (1, 'Not at all'),
+                                       (2, 'Not Really'),
+                                       (3, 'Somewhat'),
+                                       (4, 'Relatively'),
+                                       (5, 'Mostly'),
+                                       (6, 'Yes')],
                                    widget=forms.RadioSelect
                                    )
+
     question_4 = forms.ChoiceField(label="Q4: Do you feel as if management is representative of the demographics within your company?",
                                    choices=[
-                                       ('no', 'Not at all'),
-                                       ('notreally', 'Not Really'),
-                                       ('somewhat', 'Somewhat'),
-                                       ('relatively', 'Relatively'),
-                                       ('mostly', 'Mostly'),
-                                       ('yes', 'Yes')],
+                                       (1, 'Not at all'),
+                                       (2, 'Not Really'),
+                                       (3, 'Somewhat'),
+                                       (4, 'Relatively'),
+                                       (5, 'Mostly'),
+                                       (6, 'Yes')],
                                    widget=forms.RadioSelect
                                    )
-    question_5 = forms.ChoiceField(label="Q5: Do you feel as if management is representative of the demographics within your company?",
+
+    question_5 = forms.ChoiceField(label="Q5:How often do you hear of or witness individuals being bullied at your company?",
                                    choices=[
-                                       ('no', 'Not at all'),
-                                       ('notreally', 'Not Really'),
-                                       ('somewhat', 'Somewhat'),
-                                       ('relatively', 'Relatively'),
-                                       ('mostly', 'Mostly'),
-                                       ('yes', 'Yes')],
+                                       (1, 'Never'),
+                                       (2, 'Rarely'),
+                                       (3, 'Sometimes'),
+                                       (4, 'Frequently'),
+                                       (5, 'Often'),
+                                       (6, 'Daily')],
                                    widget=forms.RadioSelect
                                    )
+
+    question_6 = forms.ChoiceField(label="Q6: Are inappropriate jokes ever made within your company that might discriminate against someone based on their gender?",
+                                   choices=[
+                                       (1, 'Never'),
+                                       (2, 'Rarely'),
+                                       (3, 'Sometimes'),
+                                       (4, 'Frequently'),
+                                       (5, 'Often'),
+                                       (6, 'Daily')],
+                                   widget=forms.RadioSelect
+                                   )
+
+    question_7 = forms.ChoiceField(label="Q7: How would you rate the company culture?",
+                                   choices=[
+                                       (1, 'Terrible'),
+                                       (2, 'Sub-par'),
+                                       (3, 'Okay'),
+                                       (4, 'Not bad'),
+                                       (5, 'Pretty good'),
+                                       (6, 'Very good')],
+                                   widget=forms.RadioSelect
+                                   )
+
+    question_8 = forms.ChoiceField(label="Q8: How often do you experience or witness social exclusion based on gender?",
+                                   choices=[
+
+                                       (1, 'Never'),
+                                       (2, 'Rarely'),
+                                       (3, 'Sometimes'),
+                                       (4, 'Frequently'),
+                                       (5, 'Often'),
+                                       (6, 'Daily')],
+                                   widget=forms.RadioSelect
+                                   )
+
+    question_9 = forms.ChoiceField(label="Q9: Do you feel as if there is a gender pay gap at your workplace?",
+                                   choices=[
+                                       (1, 'Not at all'),
+                                       (2, 'Not Really'),
+                                       (3, 'Somewhat'),
+                                       (4, 'Relatively'),
+                                       (5, 'Mostly'),
+                                       (6, 'Yes')],
+                                   widget=forms.RadioSelect
+                                   )
+
+    question_10 = forms.CharField(
+        label="Anything else you'd like to add?", max_length=200)
--- hackathon/views.py
+++ hackathon/views.py
@@ -1,7 +1,6 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
-from hackathon.models import Companies, Users
 from .forms import InputForm
 from .models import Companies, Users
 
@@ -20,16 +19,25 @@ def user_form(request):
 
         if form.is_valid():
 
-            company = Companies.objects.get(stock_code='AGL')
+            company = Companies.objects.get(
+                stock_code=form.cleaned_data['company_name'])
 
-            print(company.stock_code)
             user = Users()
+
             user.stock_code = company
+            user.role = form.cleaned_data['role']
+            user.gender = form.cleaned_data['gender']
+            user.salary_band = form.cleaned_data['salary_band']
             user.Q1 = form.cleaned_data['question_1']
             user.Q2 = form.cleaned_data['question_2']
             user.Q3 = form.cleaned_data['question_3']
             user.Q4 = form.cleaned_data['question_4']
             user.Q5 = form.cleaned_data['question_5']
+            user.Q6 = form.cleaned_data['question_6']
+            user.Q7 = form.cleaned_data['question_7']
+            user.Q8 = form.cleaned_data['question_8']
+            user.Q9 = form.cleaned_data['question_9']
+            user.Q10 = form.cleaned_data['question_10']
 
             user.save()
 
@@ -41,7 +49,7 @@ def user_form(request):
         context = {}
         context['form'] = InputForm()
         return render(request, "user_form.html", context)
-    
+        
 def company(request):
     form_data = Users.objects.all()
     comp_data = Companies.objects.all()

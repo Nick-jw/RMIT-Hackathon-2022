a111721f688a91b4a3816b42cf02a3d3dd610773
Kai Gibson <kai.gib@tuta.io>
2022-06-21 10:13:50 +1000

    form working

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
+from .forms import InputForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,5 +13,15 @@ def index(request):
 
 
 def user_form(request):
-    return render(request, 'user_form.html')
+    if request.method == "POST":
+        form = InputForm(request.POST)
 
+        if form.is_valid():
+            print(form.cleaned_data)
+            for i in form.cleaned_data:
+                print(i)
+            return HttpResponseRedirect('/admin/')
+    else:
+        context = {}
+        context['form'] = InputForm()
+        return render(request, "user_form.html", context)
